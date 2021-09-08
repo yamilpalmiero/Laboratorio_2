2ae5172d0e9caa9007260ca9165ea5e09cb2aac8
Yamil Palmiero <yamilpalmiero@live.com>
2021-09-07 22:12:28 -0300

    A contar palabras

---
+++ Ejercicios/¡A contar palabras!/Vista/FrmContador.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Vista
+{
+    partial class FrmContador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbTexto = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbTexto
+            // 
+            this.rtbTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbTexto.Location = new System.Drawing.Point(5, 6);
+            this.rtbTexto.Name = "rtbTexto";
+            this.rtbTexto.Size = new System.Drawing.Size(616, 502);
+            this.rtbTexto.TabIndex = 0;
+            this.rtbTexto.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalcular.Location = new System.Drawing.Point(480, 527);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(140, 38);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmContador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 581);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rtbTexto);
+            this.Name = "FrmContador";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbTexto;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Ejercicios/¡A contar palabras!/Vista/FrmContador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class FrmContador : Form
+    {
+        public FrmContador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> contadorPalabras = ObtenerContadorPalabras();
+
+            List<KeyValuePair<string, int>> podio = contadorPalabras.ToList();
+
+            podio.Sort(CompararCantidadRepeticiones);
+            MostrarPodio(podio);
+        }
+
+
+
+
+        //METODOS
+        private Dictionary<string, int> ObtenerContadorPalabras()
+        {
+            string texto = rtbTexto.Text;
+            string[] palabras = texto.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> contadorPalabras = new Dictionary<string, int>();
+
+            foreach (string palabra in palabras)
+            {
+                if (contadorPalabras.ContainsKey(palabra))
+                    contadorPalabras[palabra]++;
+
+                else
+                    contadorPalabras.Add(palabra, 1);
+            }
+
+            return contadorPalabras;
+        }
+
+        private int CompararCantidadRepeticiones(KeyValuePair<string, int> primerElemento, KeyValuePair<string, int> segundoElemento)
+        {
+            return segundoElemento.Value - primerElemento.Value;
+        }
+
+        private void MostrarPodio(List<KeyValuePair<string, int>> podio)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (podio.Count == 0)
+                sb.AppendLine("Nose ingresaron palabras");
+            else
+            {
+                for (int i = 0; i < 3 && i < podio.Count; i++)
+                {
+                    KeyValuePair<string, int> par = podio[i];
+                    sb.AppendLine($"Palabra: {par.Key} - Cantidad: {par.Value}");
+                }
+            }
+
+            MessageBox.Show(sb.ToString(), "Podio");
+        }
+    }
+}
