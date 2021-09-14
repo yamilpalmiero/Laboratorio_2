1ef62b4730fb1f16f0ab2007ba778f09b1cb9d1e
Yamil Palmiero <yamilpalmiero@live.com>
2021-09-13 22:25:29 -0300

    Rompe

---
+++ Ejercicios/Consultaste el indice/Biblioteca/Libro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteca
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i <= paginas.Count)
+                    return paginas[i];
+                else
+                    return "";
+            }
+            set
+            {
+                if (i < paginas.Count)
+                    paginas[i] = value;
+
+                else if (i == paginas.Count)
+                    paginas.Add(value);
+            }
+        }
+    }
+}
---
+++ Ejercicios/Consultaste el indice/Vista/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Biblioteca;
+
+namespace Vista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+
+            libro[0] = "Pagina Uno";
+            libro[1] = "Pagina Dos";
+            libro[2] = "Pagina Tres";
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(libro[i]);
+            }
+        }
+    }
+}
