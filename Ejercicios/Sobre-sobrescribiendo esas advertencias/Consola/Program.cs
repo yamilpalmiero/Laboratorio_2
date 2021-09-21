62dff2dbc10b5a67f54f4176011eda7343e7c3ac
Yamil Palmiero <yamilpalmiero@live.com>
2021-09-20 21:39:11 -0300

    Terminado OK

---
+++ Ejercicios/Sobre-sobrescribiendo esas advertencias/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio No38 Guía 2017";
+            Sobrescrito sobrecarga = new Sobrescrito();
+
+            Console.WriteLine(sobrecarga.ToString());
+
+            string objeto = "¡Este es mi método ToString sobrescrito!";
+
+            Console.WriteLine("----------------------------------------------");
+            Console.Write("Comparación Sobrecargas con String: ");
+            Console.WriteLine(sobrecarga.Equals(objeto));
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(sobrecarga.GetHashCode());
+        }
+    }
+}
---
+++ Ejercicios/Sobre-sobrescribiendo esas advertencias/Consola/Sobrescrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Sobrescrito
+    {
+        public override string ToString()
+        {
+            return $"Este es mi metodo ToString sobrescrito!";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == this.GetType();
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510181;
+        }
+    }
+}
